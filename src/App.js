388157e4ab1ed9186f60b11446c48045d41b812f
etrapets <etrapets@gmail.com>
2022-01-19 18:13:32 +0200

    added typo

--- src/App.js
+++ src/App.js
@@ -7,7 +7,7 @@ function App() {
       <header className="App-header">
         <img src={logo} className="App-logo" alt="logo" />
         <p>
-            CHANGEEE
+            I DID IT !!!!!!!!
         </p>
         <a
           className="App-link"
